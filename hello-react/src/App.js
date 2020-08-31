5442f77e923623e0591fe38847f16b876d09e992
Eunmi-jo <whdmal2512@gmail.com>
2020-09-01 03:43:59 +0900

    2.4.5-4 undefined를 렌더링하지 않기-name 값이 undefined일 때

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
-const name = undefined;
-return <div>{name}</div>;
+  const name = undefined;
+  return <div>{name || '리액트'}</div>;
 }
 
 export default App;
