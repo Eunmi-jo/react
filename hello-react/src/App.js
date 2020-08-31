6aac1624067e98e3ed32e6c5815680b68480b9bc
Eunmi-jo <whdmal2512@gmail.com>
2020-09-01 02:35:25 +0900

    2.4.1-4

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';    //div 요소를 사용하지 않고, Fragment를 사용하는 경우.
+import React from 'react';        //Fragment를 사용한 다른 경우.
  
 function App() {
   return (
-    <Fragment>
+    <>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
-    </Fragment>
+    </>
   );
 }
  
