231d9b869fbb8ce2e9d3c316ba8deeec3165a15b
Eunmi-jo <whdmal2512@gmail.com>
2020-09-01 03:05:19 +0900

    2.4.4-2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
- 
+import React from ‘react‘;
+
 function App() {
-  const name = '뤼왝트';
-  return <div>{name === '리액트' ? <h1>리액트입니다.</h1> : null}</div>;        
-}         //null을 레더링하면 아무것도 보여 주지 않음.
- 
+  const name = ‘뤼왝트‘;
+  return <div>{name === ‘리액트‘ && <h1>리액트입니다.</h1>}</div>;
+}
+//AND연산자(&&)를 이용하여 레더링하기.
 export default App;
