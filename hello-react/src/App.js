1d073dbe95cfac56b0c81a9d1ef8df8ec85d3aed
Eunmi-jo <whdmal2512@gmail.com>
2020-09-01 14:02:37 +0900

    3.3.3-1 props 기본값 설정 : defaultProps-props값을 따로 지정하지 않았을 때

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name="React" />;
+  return <MyComponent />; //props값을 따로 지정하지 않았을 때
 };
 
 export default App;
