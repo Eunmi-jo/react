de9126b2428a1e00b480ebf40a8480a878229d70
Eunmi-jo <whdmal2512@gmail.com>
2020-09-01 14:31:39 +0900

    3.3.6-2 propTypes를 통한 props 검증 - name값은 무조건 문자열 형태로 전달.

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -2,4 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MyComponent = ({ name, children }) => {
-(…)
+  return (...);
+};
+
+
+MyComponent.defaultProps = {
+  name: '기본 이름'
+};
+
+
+MyComponent.propTypes = {
+  name: PropTypes.string
+};
+
+
+export default MyComponent;
