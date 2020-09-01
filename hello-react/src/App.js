1142714a673d29242b29c3bdeb4d26cb8096ccee
Eunmi-jo <whdmal2512@gmail.com>
2020-09-01 14:12:23 +0900

    3.3.4 태크 사이의 내용을 보여주는 children

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent />; //props값을 따로 지정하지 않았을 때
+  return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
