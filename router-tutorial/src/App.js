31a5fb3ad0e01115a90a06874e313c012d159331
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-03 20:11:04 +0900

    13.5-2 서브 라우트 - App.js

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
         <li>
           <Link to="/profile/gildong">gildong 프로필</Link>
@@ -24,7 +24,7 @@ const App = () => {
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles/:username" component={Profiles} />
     </div>
   );
 };
