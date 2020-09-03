8870547575ebeb4277f40cb8a2ac02723ad2a9e2
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-04 00:47:47 +0900

    16.2.5 초깃값 설정 - index.js

--- vanilla-redex/index.js
+++ vanilla-redex/index.js
@@ -3,6 +3,7 @@ const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
 
+//액션 타입과 액션 생성 함수 정의
 const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
@@ -10,3 +11,9 @@ const DECREASE = 'DECREASE';
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
+
+//초깃값 설정
+const initialState = {
+  toggle: false,
+  counter: 0
+};
