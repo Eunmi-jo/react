84a9e44bb6ec379b9130668db4b874299bf6ba9e
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-04 01:27:14 +0900

    17.3.1.2 액션 생성 함수 만들기

--- react-redux-tutorial/modules/counter.js
+++ react-redux-tutorial/modules/counter.js
@@ -1,3 +1,7 @@
 //액션 타입 정의하기
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
+
+//액션 생성 함수 만들기
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
