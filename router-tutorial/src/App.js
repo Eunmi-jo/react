9b00227c73781751f500decb89eb302b7293b854
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-03 20:21:01 +0900

    13.6.1-2 history - App.js

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
@@ -18,6 +19,9 @@ const App = () => {
           <Link to="/profiles">프로필</Link>
         </li>
         <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+        <li>
           <Link to="/profile/gildong">gildong 프로필</Link>
         </li>
       </ul>
@@ -25,6 +29,7 @@ const App = () => {
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
       <Route path="/profiles/:username" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 };
