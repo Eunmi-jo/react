2637fd44eeba1261e9ee9f047c445301acfaed2d
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-04 02:18:26 +0900

    17.6.1.1-1 counter 모듈에 적용하기 - modules/counter.js

--- react-redux-tutorial/modules/counter.js
+++ react-redux-tutorial/modules/counter.js
@@ -1,6 +1,11 @@
+import { createAction } from 'redux-actions';
+
 //액션 타입 정의하기
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 //액션 생성 함수 만들기
 export const increase = () => ({ type: INCREASE });
