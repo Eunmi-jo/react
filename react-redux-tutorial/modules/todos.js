aee1c51a434aa64f809d3be9ccfa27b0b8c4c7bb
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-04 02:22:40 +0900

    17.6.1.2-1 todos 모듈에 적용하기 - modules/todos.js

--- react-redux-tutorial/modules/todos.js
+++ react-redux-tutorial/modules/todos.js
@@ -1,8 +1,22 @@
+import { createAction } from 'redux-actions';
+
 // 액션 타입 정의하기
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
 const TOGGLE = 'todos/TOGGL'; // todo를 체크/체크 해제함
 const REMOVE = 'todos/REMOVE'; // todo를 제거함
+
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+
+let id = 3; // insert가 호출될 때마다 1씩 더해집니다.
+export const insert = createAction(INSERT, text => ({
+  id: id++,
+  text,
+  done: false,
+}));
+
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
 
 // 액션 생성 함수 만들기
 export const changeInput = input => ({
