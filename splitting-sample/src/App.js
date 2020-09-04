ce1fd5a1dca00e09f875f587fce146c7e2fc3304
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-04 16:21:34 +0900

    19.2.3-2 Loadable Components를 통한 코드 스플리팅 - App.js – loadable 사용 부분

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import loadable from '‘@loadable/component';
-const SplitMe = loadable(() => import('./SplitMe'));
+
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading…</div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
