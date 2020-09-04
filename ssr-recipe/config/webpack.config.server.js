9f4561147524d1f7a5149e6567dfcb9a5eddb067
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-04 17:52:49 +0900

    20.3.2-5 서버 사이드 렌더링 전용 웹팩 환경 설정 작성하기 - config/webpack.config.server.js

--- ssr-recipe/config/webpack.config.server.js
+++ ssr-recipe/config/webpack.config.server.js
@@ -1,3 +1,4 @@
+const nodeExternals = require('webpack-node-externals');
 const paths = require('./paths');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent'); // CSS Module의 고유 className을 만들 때 필요한 옵션
 
@@ -123,5 +124,6 @@ module.exports = {
             },
             resolve: {
                 modules: ['node_modules']
-            }
+            },
+            externals: [nodeExternals()]
 };
