943ee8307389e86fdcf051aa75a76766d3520d76
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-06 02:51:34 +0900

    26.1.2-5 API 연동하기 - pages/PostPage.js

--- blog/blog-frontend/src/pages/PostPage.js
+++ blog/blog-frontend/src/pages/PostPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
-import PostViewer from '../components/post/PostViewer';
+import PostViewerContainer from '../containers/post/PostViewerContainer';
 
 const PostListPage = () => {
     return (
         <>
             <HeaderContainer />
-            <PostViewer />
+            <PostViewerContainer />
         </>
     );
 };
