bffd39ed872a022da7aa976893e29d03c5e33083
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-05 19:19:14 +0900

    24.2.1 UI 준비하기 - 예시

---
+++ blog/blog-frontend/src/components/base/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div``;
+
+const Header = () => {
+    return (
+        <HeaderBlock>
+
+        </HeaderBlock>
+    );
+};
+
+export default Header.js;
