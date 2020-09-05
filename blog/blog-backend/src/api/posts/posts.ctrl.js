a2fb2a01a2ee8da7dd1e33ed3cbfd16426cce856
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-05 18:10:15 +0900

    23.5.5-3 포스트 수정 및 삭제 시 권한 확인하기 - src/api/posts/posts.ctrl.js - read

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -100,17 +100,7 @@ export const list = async ctx => {
   GET /api/posts/:id
 */
 export const read = async ctx => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404; // Not Found
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
+  ctx.body = ctx.state.post;
 };
 
 /*
