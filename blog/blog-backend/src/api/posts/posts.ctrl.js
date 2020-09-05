e451884276f523b5f0a6215a49b35b08f6564cfa
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-06 04:14:06 +0900

    26.2.3-2 HTML필터링하기 - src/api/posts/posts.ctrl.js - list

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -65,6 +65,15 @@ export const write = async ctx => {
   }
 };
 
+
+// html을 없애고 내용이 너무 길면 200자로 제한하는 함수
+const removeHtmlAndShorten = body => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
+
 /*
   GET /api/posts?username=&tag=&page=
 */
@@ -96,8 +105,7 @@ export const list = async ctx => {
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map(post => ({
       ...post,
-      body:
-        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+      body: removeHtmlAndShorten(post.body),
     }));
   } catch (e) {
     ctx.throw(500, e);
