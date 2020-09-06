58805c8263d3e1c020fbf290827aee55f8dd46cf
Eunmi-jo <whdmsal2512@gmail.com>
2020-09-06 11:52:27 +0900

    27.1.2-1 수정 버튼 클릭 시 글쓰기 페이지로 이동하기 - modules/write.js

--- blog/blog-frontend/src/modules/write.js
+++ blog/blog-frontend/src/modules/write.js
@@ -14,7 +14,7 @@ const [
     WRITE_POST_SUCCESS,
     WRITE_POST_FAILURE,
 ] = createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
-
+const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST';
 
 
 
@@ -28,7 +28,7 @@ export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
     body,
     tags,
 }));
-
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post);
 
 
 // 사가 생성
@@ -44,6 +44,7 @@ const initialState = {
     tags: [],
     post: null,
     postError: null,
+    originalPostId: null,
 };
 
 
@@ -70,6 +71,13 @@ const write = handleActions(
         [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
             ...state,
             postError,
+        }),
+        [SET_ORIGINAL_POST]: (state, { payload: post }) => ({
+            ...state,
+            title: post.title,
+            body: post.body,
+            tags: post.tags,
+            originalPostId: post._id,
         }),
     },
     initialState,
